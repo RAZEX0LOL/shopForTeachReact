a0320e0bee9360b8910806daa576fa6b5f297e2e
RAZEX0LOL <khattayev00@mail.ru>
2023-10-13 20:36:24 +0400

    add react icons and add styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import {GoTrash} from "react-icons/go"
 
 export default function Header(){
+    let[cartOpen,setCartOpen]=useState(false);
+
     return(
         <header>
             <div>
@@ -10,7 +13,9 @@ export default function Header(){
                 <li>Про нас</li>
                 <li>Контакты</li>
                 <li>Кабинет</li>
+                <li>Скачать прайс</li>
             </ul>
+            <GoTrash onClick={()=>setCartOpen(cartOpen=!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
             <div className="presentation"></div>
         </header>
     );
