0240b267d798d6dd114eafaa0872920c4344ee73
RAZEX0LOL <khattayev00@mail.ru>
2023-10-23 21:00:50 +0400

    add styles, add json-server and connect

--- src/App.js
+++ src/App.js
@@ -4,99 +4,30 @@ import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Categories from "./components/Categories";
 import ShowFullItem from "./components/ShowFullItem";
+import axios from "axios";
 
 export default function App() {
   const [orders, setOrders] = useState([]);
   const [currentItems,setCurrentItems]=useState([]);
   const [showFullItem,setShowFullItem]=useState(false);
-  const [items,setItems] =useState([
-    {
-      id:1,
-      title:'iPhone 15 Pro Max',
-      img:'iPhone15ProMax.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Apple',
-      price: '1249.99'
-    },
-    {
-      id:2,
-      title:'iPhone 14 Pro Max',
-      img:'iPhone14ProMax.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Apple',
-      price: '1149.99'
-    },
-    {
-      id:3,
-      title:'Samsung S23 Ultra',
-      img:'SamsungS23Ultra.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Samsung',
-      price: '871.99'
-    },
-    {
-      id:4,
-      title:'Samsung Z Fold 5',
-      img:'SamsungZFold5.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Samsung',
-      price: '1799.99'
-    },
-    {
-      id:5,
-      title:'Nothing Phone 1',
-      img:'NothingPhone1.jpeg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Nothing',
-      price: '419.99'
-    },
-    {
-      id:6,
-      title:'Nothing Phone 2',
-      img:'NothingPhone2.png',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Nothing',
-      price: '599.99'
-    },
-    {
-      id:7,
-      title:'Huawei P60 Pro',
-      img:'HuaweiP60Pro.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Huawei',
-      price: '963.99'
-    },
-    {
-      id:8,
-      title:'Huawei Mate X3',
-      img:'HuaweiMateX3.png',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Huawei',
-      price: '2099.99'
-    },
-    {
-      id:9,
-      title:'iPhone 15 Plus',
-      img:'iPhone15Plus.jpg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Apple',
-      price: '799.99'
-    },
-    {
-      id:10,
-      title:'Samsung Z Flip 5',
-      img:'SamsungZFlip5.jpeg',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      category: 'Samsung',
-      price: '1119.99'
-    }
-  ])
+  const [fullItem,setFullItem]=useState({});
+  const [items,setItems] =useState([]);
 
   useEffect(() => {
-    setCurrentItems(items);
+    axios
+      .get("http://localhost:3001/items")
+      .then((response)=>{
+        setItems(response.data);
+        chooseCategory("all");
+        setCurrentItems(response.data);
+      })
+      .catch((error)=>{
+        console.error("Ошибка при загрузке данных", error);
+      });
   }, [items]);
   
   const addToOrder = (item3) => { 
+  
     if (!orders.some((el) => el.id === item3.id)) {
       setOrders([...orders, item3]);
     }
@@ -116,6 +47,7 @@ export default function App() {
   }
 
   const onShowItem = (item)=>{
+    setFullItem(item);
     setShowFullItem(!showFullItem);
   }
 
@@ -124,7 +56,7 @@ export default function App() {
       <Header orders={orders} onDelete={deleteOrder}/>
       <Categories chooseCategory={chooseCategory}/>
       <Items allItems={currentItems} onShowItem={onShowItem} onAdd={addToOrder}/>
-      {showFullItem && <ShowFullItem/>}
+      {showFullItem && <ShowFullItem onShowItem={onShowItem} onAdd={addToOrder} item={fullItem}/>}
       <Footer/>
     </div>
   );
